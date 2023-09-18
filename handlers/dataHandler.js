ce19f9aae2a42971b986ad601352c9269b8920d6
Taha_arch <taha.elatoui.03@gmail.com>
2023-09-18 16:10:03 +0100

    the original state

--- handlers/dataHandler.js
+++ handlers/dataHandler.js
@@ -14,19 +14,3 @@ module.exports = {
     readDataFromFile,
     writeDataToFile
 };
-const fs = require('fs');
-
-// implement readData function
-const readDataFromFile = (filePath) => {
-    
-};
-
-// implement writeData function
-const writeDataToFile = (filePath, data) => {
-    
-};
-
-module.exports = {
-    readDataFromFile,
-    writeDataToFile
-};
